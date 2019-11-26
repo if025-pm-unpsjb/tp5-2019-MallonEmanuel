fad36b8f5488607d20f9af1782d834fe8d429fee
emanuel <emanuelsm18@gmail.com>
2019-11-25 21:50:20 -0300

    ejercicio 1 funcionando

--- main1.cpp
+++ main1.cpp
@@ -3,12 +3,18 @@
 #include "FreeRTOS.h"   // Definiciones principales de FreeRTOS
 #include "task.h"       // Funciones para el control de las tareas
 
+Serial pc(USBTX, USBRX); // tx, rx
+
 struct sTaskParams{
-	uint32_t id;
-	uint32_t c;
-	uint32_t d;
-	uint32_t t;
+	int id;
+	int c;
+	int d;
+	int t;
 };
+
+sTaskParams taskParams[3] =  { { 0, 2, 4, 4 }, { 1, 1, 5, 5 },
+		{ 2, 1, 6, 6 } };
+
 
 void thread1 (void*);
 /* Implementar los siguientes conjuntos de tareas con FreeRTOS, bajo la polıtica
@@ -19,9 +25,7 @@ int main(){
 
 	// Initializes the trace recorder, but does not start the tracing.
 	vTraceEnable( TRC_INIT );
-
-
-	struct sTaskParams taskParams[4];
+/*	struct sTaskParams taskParams[4];
 	taskParams[0].id = 0;
 	taskParams[0].c = 2;
 	taskParams[0].d = 4;
@@ -36,52 +40,54 @@ int main(){
 	taskParams[2].c = 1;
 	taskParams[2].d = 6;
 	taskParams[2].t = 6;
-
+*/
 	int limit = 2;
 
-/*	for (int i = 0; i <= limit ; i++){
+	for (int i = 0; i <= limit ; i++){
 		char name[10] = "";
-		sprintf(name,"t_%d",i);
+		sprintf(name,"T%d",i);
 		xTaskCreate(thread1 					// Function that implements the task.
 					,name						// Text name for the task.
-					,128						// Stack size in words, not bytes.
+					,256						// Stack size in words, not bytes.
 					,(void*) &taskParams[i]		// Parameter passed into the task.
 					,configMAX_PRIORITIES - (i+1)	// Priority at which the task is created.
 					,NULL);						// Used to pass out the created task's handle.
 	}
-*/
 
+/*
 	xTaskCreate(thread1 					// Function that implements the task.
 					,"Tarea_A"						// Text name for the task.
 					,256						// Stack size in words, not bytes.
 					,(void*) &taskParams[0]		// Parameter passed into the task.
-					,configMAX_PRIORITIES -1	// Priority at which the task is created.
+					,configMAX_PRIORITIES -2	// Priority at which the task is created.
 					,NULL);						// Used to pass out the created task's handle.
 
 	xTaskCreate(thread1 					// Function that implements the task.
 						,"Tarea_I"						// Text name for the task.
-						,128						// Stack size in words, not bytes.
+						,256						// Stack size in words, not bytes.
 						,(void*) &taskParams[1]		// Parameter passed into the task.
-						,configMAX_PRIORITIES -2	// Priority at which the task is created.
+						,configMAX_PRIORITIES -3	// Priority at which the task is created.
 						,NULL);						// Used to pass out the created task's handle.
 
 	xTaskCreate(thread1 					// Function that implements the task.
 							,"TAREA_Z"						// Text name for the task.
-							,128						// Stack size in words, not bytes.
+							,256						// Stack size in words, not bytes.
 							,(void*) &taskParams[2]		// Parameter passed into the task.
-							,configMAX_PRIORITIES -3 	// Priority at which the task is created.
+							,configMAX_PRIORITIES -4 	// Priority at which the task is created.
 							,NULL);						// Used to pass out the created task's handle.
+*/
 	// Starts the tracing.
 	vTraceEnable( TRC_START );
+	pc.printf("Iniciando simulación\n\r");
 
     vTaskStartScheduler();
 
     for( ;; );
 }
 
-void eat_cpu(int time){
+void eat_cpu(uint32_t time){
 	float res = 0;
-	for(uint32_t i = 0;i <= time * 100; i++){
+	for(uint32_t i = 0;i <= time ; i++){
 		for(uint32_t j; j <= 500 ; j++){
 			res = j * (j+1) * 100 + (100 *10 * 2);
 			res = j * (j+1) * 100 + (100 *10 * 2);
@@ -95,12 +101,13 @@ void thread1 (void* params){
     taskParams = (struct sTaskParams *) params;
 
     // Definicion de variables locales
-    const uint32_t time   		  = (uint32_t) taskParams->c;
-    const portTickType xFrequency = (uint32_t) taskParams->t;
+    const portTickType time   	  =  taskParams->c * 1000;
+    const portTickType xFrequency =  taskParams->t * 1000;
     portTickType xLastWakeTime = 0;
 
     while(1){
-	    eat_cpu(time);
+    	pc.printf("Ejecutando tarea %d durante %d\n\r",taskParams->id,taskParams->c);
 	    vTaskDelayUntil( &xLastWakeTime, xFrequency );
     }
 }
+
